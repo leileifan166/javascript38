3f1e4bb4f3f69cf7097740beee5dc360e9366128
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2022-01-03 12:07:29 +0900

    Add files via upload

---
+++ javascript38/myjs.js
@@ -0,0 +1,16 @@
+var object = {
+    user: "太郎",
+    sex: "男",
+    age: 29,
+}
+
+console.log(object);
+for(var k in object){
+    console.log(object[k]);
+    /* console.log(k); */
+}
+var string = "1234556"
+document.write(typeof String);
+document.write(typeof string);
+
+
